e5dbc4992279c57025e3f7be715631fc1ab12440
Laptopkaran <shadighanaati@gmail.com>
2024-03-31 14:54:43 +0330

    add login and signup

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('aboutus/', views.AboutUsPageView.as_view(), name='aboutus')
+    path('aboutus/', views.AboutUsPageView.as_view(), name='aboutus'),
 ]
